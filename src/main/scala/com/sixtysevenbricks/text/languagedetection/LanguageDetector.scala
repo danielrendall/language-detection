875a6fdcd6033f2c9443256a6cbbd72ad3f94668
Daniel Rendall <drendall@gmail.com>
2021-10-13 22:49:58 +0100

    Allow languages to be added and removed at runtime.

--- src/main/scala/com/sixtysevenbricks/text/languagedetection/LanguageDetector.scala
+++ src/main/scala/com/sixtysevenbricks/text/languagedetection/LanguageDetector.scala
@@ -19,14 +19,54 @@ class LanguageDetector(initialFingerprints: Iterable[LanguageFingerprint]) {
   private final val fingerprints =
     new AtomicReference[Map[String, LanguageFingerprint]](initialFingerprints.map(s => s.languageName -> s).toMap)
 
+  private val Zero: (String, Int) = ("", Int.MinValue)
+
   /** Identify a text as being in a specific language, returning the name of that language. */
   def identifyLanguage(text: String): String = {
     val maxChars = 4
 
     val profile = NGramProfiler.createNgramProfile(text, maxChars).take(300)
 
-    fingerprints.get().values.map(lf => (lf.languageName, lf.computeDistance(profile, maxChars))).max._1
+    // We prepend zero to ensure this returns an empty string if there are no configured languages
+    (Zero :: fingerprints.get().values.map(lf => (lf.languageName, lf.computeDistance(profile, maxChars))).toList).max._1
   }
+
+  /**
+   * Add or replace an existing fingerprint (this happens atomically; anything currently being processed will still use
+   * the old collection of fingerprints). Note that this mutates the current language detector!
+   * @param newFingerprint The new language fingerprint which will replace an existing one if the language matches
+   * @return True if we replaced an existing fingerprint, otherwise false
+   */
+  def addOrUpdateFingerprint(newFingerprint: LanguageFingerprint): Boolean = {
+    fingerprints.synchronized {
+      val currentMap = fingerprints.get()
+      val ret = currentMap.contains(newFingerprint.languageName)
+      fingerprints.set(currentMap.updated(newFingerprint.languageName, newFingerprint))
+      ret
+    }
+  }
+
+  /**
+   * Remove a fingerprint for the given language. Note that this mutates the current language detector!
+   * @param language Name of the language to remove
+   * @return True if we removed the fingerprint, otherwise false
+   */
+  def removeFingerprint(language: String): Boolean = {
+    fingerprints.synchronized {
+      val currentMap = fingerprints.get()
+      val ret = currentMap.contains(language)
+      if (ret) {
+        fingerprints.set(currentMap.-(language))
+      }
+      ret
+    }
+  }
+
+  /**
+   * Return the languages that we currently know about.
+   * @return
+   */
+  def getAvailableLanguages: Seq[String] = fingerprints.get().keys.toList.sorted
 }
 
 object LanguageDetector {
@@ -37,12 +77,17 @@ object LanguageDetector {
   val availableLanguages: Seq[String] = Seq("de-DE", "en-US", "fr-FR")
 
   /**
+   * Return a LanguageDetector with no initial languages (
+   */
+  def newEmpty: LanguageDetector = new LanguageDetector(Seq.empty)
+
+  /**
    * Build a language detector initialised with fingerprints taken from the resources directory; these are currently
    * de-DE, en-US and fr-FR
    *
    * @return A default LanguageDetector
    */
-  def default: LanguageDetector = {
+  def newDefault: LanguageDetector = {
     val fingerprints: Seq[LanguageFingerprint] = availableLanguages.map { l =>
       LanguageFingerprint.load(l, LanguageDetector.getClass.getResourceAsStream(s"/profile/$l.fp"))
     }
--- src/test/scala/com/sixtysevenbricks/text/languagedetection/LanguageDetectorSpec.scala
+++ src/test/scala/com/sixtysevenbricks/text/languagedetection/LanguageDetectorSpec.scala
@@ -3,20 +3,43 @@ package com.sixtysevenbricks.text.languagedetection
 import org.specs2.mutable.Specification
 
 class LanguageDetectorSpec extends Specification {
-  val detector: LanguageDetector = LanguageDetector.default
 
   "Language detector" should {
 
     "identifyEnglish" in {
-      detector.identifyLanguage("Hello world this is English") must_=== "en-US"
+      LanguageDetector.newDefault.identifyLanguage("Hello world this is English") must_=== "en-US"
     }
 
     "identifyFrench" in {
-      detector.identifyLanguage("Calcul des structures pour leur ") must_=== "fr-FR"
+      LanguageDetector.newDefault.identifyLanguage("Calcul des structures pour leur ") must_=== "fr-FR"
     }
 
     "identifyGerman" in {
-      detector.identifyLanguage("Bemessung und Konstruktion von Stahlbauten") must_=== "de-DE"
+      LanguageDetector.newDefault.identifyLanguage("Bemessung und Konstruktion von Stahlbauten") must_=== "de-DE"
+    }
+  }
+
+  "Adding and removing languages" should {
+
+    "return blank if no language is configured" in {
+      val detector = LanguageDetector.newEmpty
+      detector.identifyLanguage("Hello world this is English") must_=== ""
+    }
+
+    "allow a language to be added" in {
+      val detector = LanguageDetector.newEmpty
+      detector.addOrUpdateFingerprint(LanguageFingerprint.load("fr-FR",
+        LanguageDetector.getClass.getResourceAsStream("/profile/fr-FR.fp")))
+      // If there's only one language, everything will look like that language
+      detector.identifyLanguage("Hello world this is English") must_=== "fr-FR"
+    }
+
+    "allow a language to be removed" in {
+      val detector = LanguageDetector.newDefault
+      detector.identifyLanguage("Calcul des structures pour leur ") must_=== "fr-FR"
+      detector.removeFingerprint("fr-FR")
+      // If French is not an option, this apparently looks more like German than English
+      detector.identifyLanguage("Calcul des structures pour leur ") must_=== "de-DE"
     }
   }
 }
